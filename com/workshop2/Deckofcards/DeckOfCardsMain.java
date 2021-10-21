898fd65ac0d9e5ec155fbb3d0d5bac458fdfd40b
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-21 12:12:57 +0530

    Added:Initialize size of deck of cards

--- com/workshop2/Deckofcards/DeckOfCardsMain.java
+++ com/workshop2/Deckofcards/DeckOfCardsMain.java
@@ -6,6 +6,7 @@ public class DeckOfCardsMain {
 	public static void main(String[] args) {
 		 DeckOfCardsGame deckOfCardsGame = new DeckOfCardsGame();
 	        deckOfCardsGame.welcome();
+	        deckOfCardsGame.deckOfCards();
 
 	}
 
